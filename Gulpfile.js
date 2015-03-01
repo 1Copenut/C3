a8bfa6ea112fca4779dda00e4c4d300443f8fa8c
Trevor Pierce <trevor.pierce@continuumdesign.net>
2015-03-01 14:05:36 -0600

    Adding the Gulp Sass compile task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
-    server = require('gulp-express');
+    server = require('gulp-express'),
+    sass = require('gulp-sass');
 
 gulp.task('server', function() {
    server.run(['server/server.js']); 
+});
+
+gulp.task('sass', function() {
+    gulp.src('app/styles/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./app/styles/css'));
 });
