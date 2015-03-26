e02a7ac0e9eef5350bd23166054978b9da638151
Trevor Pierce <trevor.pierce@continuumdesign.net>
2015-03-26 07:32:21 -0500

    Adding SCSS lint to the Gulpfile.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp'),
     server = require('gulp-express'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    sasslint = require('gulp-scss-lint'),
+    cache = require('gulp-cached');
 
 gulp.task('server', function() {
-   server.run(['server/server.js']); 
+    server.run(['server/server.js']); 
+    console.log('App is running on port 3000');
 });
 
 gulp.task('sass', function() {
     gulp.src('app/styles/sass/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./app/styles/css'));
+});
+
+gulp.task('sasslint', function() {
+    gulp.src('app/styles/sass/*.scss')
+        .pipe(cache('sasslint'))
+        .pipe(sasslint());
 });
