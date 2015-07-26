48b93271bec6df04eff1b293fa1ed427c0bf0a61
Trevor Pierce <trevor.pierce@continuumdesign.net>
2015-07-25 23:08:51 -0500

    Adding all files this time.

--- test/scripts/out/output.js
+++ test/scripts/out/output.js
@@ -14,7 +14,65 @@ describe('Smoke test', function() {
 }).call(this,require('_process'),typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/test/scripts/src/main.js","/test/scripts/src")
 },{"./navigation-test":44,"_process":8,"buffer":4}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
+/*global define, $:true */
+define([], function () {
+	'use strict';
+	
+	var Historical = Historical || {};
+	
+	Historical = {
+		config: {
+			links: '#navigation a',
+			list: '#navigation li'
+		},
+		
+		/* Change the URL bar in modern browsers */
+		historyEvent: function (stateObj, title, url) {
+			history.pushState(stateObj, title, url);
+		},
 
+		/* Popevent to reload previous content when the Back button is pressed */
+		popEvent: function () {
+			window.addEventListener('popstate', function(e) {
+				var targetLink = e.target.location.href,
+						shortened = targetLink.split('/'),
+						finalTarget = shortened[shortened.length-1];
+						
+				Historical.loadContent(finalTarget);
+				
+				Historical.popstateSwitchClass(finalTarget);
+			});
+		},
+		
+		/* Use the $.ajax() function to populate main content */
+		loadContent: function (targetLink) {
+			$.ajax({
+				url: 'templates/' + targetLink,
+				dataType: 'html',
+				cache: false,
+				success: function (data) {
+					$('#main').html(data);
+				}
+			});
+		},
+		
+		/* Remove the current class from all #navigation list items, and add it to the popstate href */
+		popstateSwitchClass: function(listTarget) {
+			var list = $(Historical.config.list),
+					navLinks = $(Historical.config.links);
+			
+			list.removeClass('current');
+			
+			navLinks.each(function (i) {
+				if ($(this).attr('href') === listTarget) {
+					$(this).parent().addClass('current');
+				}
+			});
+		}
+	};
+	
+	return Historical;
+});
 }).call(this,require('_process'),typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/app/scripts/src/historical.js","/app/scripts/src")
 },{"_process":8,"buffer":4}],3:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
