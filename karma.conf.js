239e534940d353c1b3818b71c3e7c76834bf03b1
Trevor Pierce <trevor.pierce@continuumdesign.net>
2015-12-01 22:56:30 -0600

    Adding proper Karma fixtures, and verifying the smoke test works.

--- karma.conf.js
+++ karma.conf.js
@@ -7,63 +7,59 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['browserify', 'mocha'],
-
+    frameworks: ['browserify', 'mocha', 'fixture'],
 
     // list of files / patterns to load in the browser
     files: [
-      'test/scripts/src/unit/*.js'
+      'test/scripts/src/unit/*.js',
+      'test/fixtures/**/*'
     ],
-
 
     // list of files to exclude
     exclude: [
     ],
 
+    // JSON preprocessor plugin
+    jsonFixturesPreprocessor: {
+        variableName: '__json__'
+    },
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'test/scripts/src/unit/*.js': [ 'browserify' ]
+        'test/scripts/src/unit/*.js': [ 'browserify' ],
+        '**/*.html': ['html2js'],
+        '**/*.json': ['json_fixtures']
     },
-
 
     browserify: {
         debug: true,
         transform: []
     },
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
--- test/scripts/src/unit/tddSmoke.js
+++ test/scripts/src/unit/tddSmoke.js
@@ -2,48 +2,27 @@
 var should = require('chai').should(),
     $ = require('jquery');
 
-/* Use BEM for naming fixtures */
-var navMenu = {
-    template:
-        '<ul class="load-content">'
-        + '<li><a href="about.html">About</a></li>'
-        + '<li><a href="services.html">Services</a></li>'
-        + '<li><a href="contact.html">Contact</a></li>'
-        + '</ul>',
-
-    render: function(el) {
-            $(el).empty().prepend(this.template);
-    }
-};
-
 describe('Navigation block with fixtures', function() {
-    var Historical, historicalTest;
+    before(function() {
+        'use strict';
+        fixture.setBase('test/fixtures');
+    });
 
     beforeEach(function() {
-       'use strict';
-
-       this.$fixture
-           .empty()
-           .prepend($('#fixtures'));
-
-       navMenu.render(this.$fixture);
-   });
-
-   before(function() {
         'use strict';
-        this.$fixture = $('<nav id="navigation" role="navigation"></nav>');
+        this.result = fixture.load('tmpl__navigation.html');
     });
 
     afterEach(function() {
         'use strict';
-        $('#fixtures').empty();
+        fixture.cleanup();
     });
     
     describe('#Nav menu', function() {
         it('Should have a length of 3', function() {
             'use strict';
 
-            var navNodes = this.$fixture.find('.load-content li'),
+            var navNodes = $(this.result).find('.load-content li'),
                 navLength = navNodes.length;
 
             navLength.should.equal(3);
