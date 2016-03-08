72d6d2984214c4c8ef1f6e4f488fe20b5beff1fd
Trevor Pierce <tpierce@boydcatongroup.com>
2016-03-08 10:00:34 -0600

    Adding full Instanbul code coverage, fixed an error in Karma config, and added browerify-istanbul and instanbul modules to package.json.

--- config/env/test.js
+++ config/env/test.js
@@ -4,6 +4,9 @@ module.exports = {
   karma: {
     browsers: ['Chrome'],
     reporters: ['mocha', 'coverage'],
+    coverageReporter: {
+      type: 'html'
+    },
     autoWatch: false,
     singleRun: true
   }
