cfb0eaed5d2526677c063d6d453da9307052c405
Trevor Pierce <tpierce@boydcatongroup.com>
2016-03-21 10:28:11 -0500

    Adding back the postcss process and Autoprefixer.

--- tasks/sass/sassBuild.js
+++ tasks/sass/sassBuild.js
@@ -3,6 +3,9 @@ module.exports = function(gulp, autoprefixer, browsersync, reload, $) {
     return function() {
         'use strict';
 
+        var processors = [
+            autoprefixer({ browsers: ['last 2 versions'] })
+        ];
         var filter = $.filter(['*.css', '!*.map'], { restore: true });
         var stream = gulp.src('app/styles/sass/*.scss')
             .pipe($.plumber())
@@ -12,6 +15,7 @@ module.exports = function(gulp, autoprefixer, browsersync, reload, $) {
             }))
             .pipe($.sourcemaps.write('./'))
             .pipe(filter)
+            .pipe($.postcss(processors))
             .pipe(filter.restore)
             .pipe(gulp.dest('app/styles/css'))
             .pipe(reload({stream: true}))
